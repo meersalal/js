0a225e71a321d00f577d738b12845a1de6fa5e6a
Meer Salal Gurmani <77038502+meersalal@users.noreply.github.com>
2024-08-19 17:16:54 +0000

    loops

---
+++ loops.js
@@ -0,0 +1,31 @@
+// break and continue
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("Detected 5");
+//         break
+//     }
+//     console.log(element);   
+    
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     const element = i;
+//     if (element == 5) {
+//         console.log("Detected 5");
+//         continue
+//     }
+//     console.log(element);   
+    
+// }
+
+
+//         do-while loop
+
+let price = 98
+
+do {
+    console.log(`Price is ${price}`);
+    price++
+} while (price <= 100);
